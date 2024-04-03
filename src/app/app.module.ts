6fa8966418e90233d96862cffdd96ddc60089b72
diarra004 <ndiayedia887@gmail.com>
2024-04-03 17:43:30 +0000

    diastar

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+
+
+
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ConnexionComponent } from './connexion/connexion.component'; 
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { EtudiantComponent } from './etudiant/etudiant.component'; // Importez AppRoutingModule
+// Importez AccueilComponent
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AccueilComponent,
+    ConnexionComponent,
+    InscriptionComponent,
+    EtudiantComponent // Ajoutez AccueilComponent aux déclarations
+  ],
+  imports: [
+    BrowserModule,
+   
+    FormsModule,
+    AppRoutingModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/connexion/connexion.component.ts
@@ -0,0 +1,12 @@
+// connexion.component.ts
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-connexion',
+  templateUrl: './connexion.component.html',
+  styleUrls: ['./connexion.component.css']
+})
+export class ConnexionComponent {
+ 
+}
